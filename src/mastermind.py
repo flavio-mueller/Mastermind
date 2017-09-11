e7a66a5fdb3bd75a03b0f01d997526d7e1879cff
Nao Pross <naopross@thearcway.org>
2017-09-11 16:58:55 +0200

    basic implementation for the main code flow
    Game flags are still missing (hints by the codemaker)

--- src/mastermind.py
+++ src/mastermind.py
@@ -12,21 +12,38 @@ PIN_SELECT = 13
 
 ## mastermind code ( global variables )
 # game variables
+solution = []
 board = [[] for i in range(MAX_ATTEMPTS)]
 attempt = 0
 
 # i/o variables
-ctrl_switches = {
+switches = {
     'next': m.Pin(PIN_NEXT, m.Pin.IN, m.Pin.PULL_DOWN),
     'select': m.Pin(PIN_SELECT, m.Pin.IN, m.Pin.PULL_DOWN),
-    '3': None,
-    '4': None
 }
 
 ## helper functions
-def 
+def refresh_matrix():
+    for row in range(attempt):
+        pass
+
+def check_victory():
+    for i in range(PEGS_COUNT):
+        if board[attempt][i] == solution[i]:
+            break
 
 # select start code
+while len(solution) != PEGS_COUNT:
+    if switches['next'].value():
+        solution.append(color)
+    elif switches['select'].value():
+        # TODO: next color
+        pass
+
+    refresh_matrix()
 
 # play 
-
+while len(attempt) < MAX_ATTEMPTS or check_victory():
+    while len(board[attempt]) < PEGS_COUNT:
+        # TODO: get user input like on the start -> helper function ?
+        pass
