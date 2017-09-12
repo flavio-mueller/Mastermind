d70355d3aaf0b69fccba9d5380888b1cf59c5016
Mcluky <luk.meili@bluewin.ch>
2017-09-12 08:28:59 +0200

    9/11 couln't have been an inside job because the planes came from the outside
    Moved to Github :)

---
+++ src/Disco.py
@@ -0,0 +1,55 @@
+from neopixel import NeoPixel
+from machine import Pin
+import random
+import time
+
+numPixel = 64
+neoPin = Pin(18,Pin.OUT)
+
+neoPixel = NeoPixel(neoPin, numPixel)
+
+button1 = Pin(12, Pin.IN)
+button2 = Pin(13, Pin.IN)
+button3 = Pin(14, Pin.IN)
+
+button1LED = Pin(25, Pin.OUT)
+button2LED = Pin(26, Pin.OUT)
+button3LED = Pin(27, Pin.OUT)
+
+button1State = 0
+
+button1Released = 0
+
+def disco():
+    neoPixel.fill((random.randrange(0, 255), random.randrange(0, 255), random.randrange(0, 255)))
+    neoPixel.write()
+    button1LED(random.randrange(0, 2))
+    button2LED(random.randrange(0, 2))
+    button3LED(random.randrange(0, 2))
+    #time.sleep(0.1)
+
+def discov2():
+    for i in range(63):
+        neoPixel[i] = (random.randrange(0, 255), random.randrange(0, 255), random.randrange(0, 255))
+    neoPixel.write()
+    #time.sleep(0.1)
+
+def checkButton1():
+    global button1State
+    global button1Released
+
+    button1StateOld = button1State
+    button1State = button1.value()
+    button1Released = button1StateOld == 1 and button1State == 0
+    if button1Released:
+        print("button1 was pressed")
+        disco()
+
+def countingUp():
+    for i in range(63):
+        neoPixel[i] = (255, 255, 255)
+        neoPixel.write()
+        time.sleep(1)
+
+while True:
+    countingUp()
--- src/mastermind.py
+++ src/mastermind.py
@@ -1,49 +1,173 @@
-import machine as m
+try:
+    from neopixel import NeoPixel
+    from machine import Pin
+    import random
+    import time
+except:
+    print("Imports are not available")
 
-## constant values
-# game settings
-PEGS_COUNT = 4
-MAX_ATTEMPTS = 16
+state_codeSetting = 0
+state_autoSet = 1
+state_codeGuessing = 2
+state_autoGuess = 3
+state_checking = 4
+state_won = 5
+state_outOfTries = 6
+state_waitForReset = 7
+state_reset = 8
 
-# GPIO settings
-PIN_NEXT = 12
-PIN_SELECT = 13
+state = state_reset
 
+numPixel = 64
+neoPin = Pin(18,Pin.OUT)
 
-## mastermind code ( global variables )
-# game variables
-solution = []
-board = [[] for i in range(MAX_ATTEMPTS)]
-attempt = 0
+neoPixel = NeoPixel(neoPin, numPixel)
 
-# i/o variables
-switches = {
-    'next': m.Pin(PIN_NEXT, m.Pin.IN, m.Pin.PULL_DOWN),
-    'select': m.Pin(PIN_SELECT, m.Pin.IN, m.Pin.PULL_DOWN),
-}
+button1 = Pin(12, Pin.IN)
+button2 = Pin(13, Pin.IN)
+button3 = Pin(14, Pin.IN)
+#button4 = Pin(15, Pin.IN)
 
-## helper functions
-def refresh_matrix():
-    for row in range(attempt):
-        pass
+button1LED = Pin(25, Pin.OUT)
+button2LED = Pin(26, Pin.OUT)
+button3LED = Pin(27, Pin.OUT)
+#button4LED = Pin(28, Pin.OUT)
 
-def check_victory():
-    for i in range(PEGS_COUNT):
-        if board[attempt][i] == solution[i]:
-            break
+button1State = 0
+button2State = 0
+button3State = 0
+#button4State = 0
 
-# select start code
-while len(solution) != PEGS_COUNT:
-    if switches['next'].value():
-        solution.append(color)
-    elif switches['select'].value():
-        # TODO: next color
-        pass
+button1Released = 0
+button2Released = 0
+button3Released = 0
+#button4Released = 0
 
-    refresh_matrix()
+colBlue = (0, 0, 255)
+colRed = (255, 0, 0)
+colGreen = (0, 255, 0)
+colYellow = (255, 255, 0)
+colTurquoise = (0, 255, 255)
+colPink = (255, 0, 255)
 
-# play 
-while len(attempt) < MAX_ATTEMPTS or check_victory():
-    while len(board[attempt]) < PEGS_COUNT:
-        # TODO: get user input like on the start -> helper function ?
-        pass
+def checkButton1():
+    global button1State
+    global button1Released
+
+    button1StateOld = button1State
+    button1State = button1.value()
+    button1Released = button1StateOld == 1 and button1State == 0
+    if button1Released:
+        print("button1 was pressed")
+
+def checkButton2():
+    global button2State
+    global button2Released
+
+    button2StateOld = button2State
+    button2State = button2.value()
+    button2Released = button2StateOld == 1 and button2State == 0
+    if button2Released:
+        print("button2 was pressed")
+
+def checkButton3():
+    global button3State
+    global button3Released
+
+    button3StateOld = button3State
+    button3State = button3.value()
+    button3Released = button3StateOld == 1 and button3State == 0
+    if button3Released:
+        print("button3 was pressed")
+
+"""def checkButton4():
+    global button4State
+    global button4Released
+
+    button4StateOld = button4State
+    button4State = button4.value()
+    button4Released = button4StateOld == 1 and button4State == 0
+    if button4Released:
+        print("button4 was pressed")
+        state = state_reset
+"""
+def codeSetting():
+    global state
+    print(state)
+    now = int(round(time.time()))
+    print("Now: ", now)
+    if now % 2 == 0:
+        button1LED(1)
+        neoPixel[1] = (255, 255, 255)
+        neoPixel.write()
+    else:
+        neoPixel[1] = (255, 255, 255)
+        neoPixel.write()
+        button1LED(0)
+    #state = state_codeGuessing
+
+def autoSet():
+    global state
+    print(state)
+    state = state_codeGuessing
+
+def codeGuessing():
+    global state
+    print(state)
+    state = state_checking
+
+def autoGuess():
+    global state
+    print(state)
+    state = state_checking
+
+def checking():
+    global state
+    print(state)
+    state = state_won
+
+def won():
+    global state
+    print(state)
+    state = state_waitForReset
+
+def outOfTries():
+    global state
+    print(state)
+    state = state_waitForReset
+
+def waitForReset():
+    global state
+    print(state)
+    state = state_reset
+
+def reset():
+    global state
+    print(state)
+    state = state_codeSetting
+    print("Game was reseted")
+
+while True:
+    checkButton1()
+    checkButton2()
+    checkButton3()
+    #checkButton4()
+
+    if state == state_codeSetting:
+        codeSetting()
+    elif state == state_autoSet:
+        autoSet()
+    elif state == state_codeGuessing:
+        codeGuessing()
+    elif state == state_autoGuess:
+        autoGuess()
+    elif state == state_checking:
+        checking()
+    elif state == state_won:
+        won()
+    elif state == state_outOfTries:
+        outOfTries()
+    elif state == state_waitForReset:
+        waitForReset()
+    elif state == state_reset:
+        reset()
