078230090f218ca3c2c61b16ad4dfa5028b882ea
Lukeesta <luke.stampfli@swissonline.ch>
2017-09-13 08:45:35 +0200

    Created a jump'n run
    Created a jump'n run that should work on the microcontroler

---
+++ src/Mastermind/ImBored.py
@@ -0,0 +1,122 @@
+try:
+    #from neopixel import NeoPixel
+    #from machine import Pin
+    import random
+    import time
+except:
+    print("Imports are not available")
+
+#neopixel
+
+#numPixel = 64
+#neoPin = Pin(18,Pin.OUT)
+#neoPixel = NeoPixel(neoPin, numPixel)
+
+#constants
+green = (0,40,0)
+white = (40,40,40)
+red = (40,0,0)
+brown = (204,102,0)
+
+#buttons
+#button0 = Pin(12, Pin.IN)
+#button1 = Pin(13, Pin.IN)
+#button2 = Pin(14, Pin.IN)
+
+colors = (white,green,red,brown)
+
+#global variables
+startTime = time.time()
+lastFrameTime = time.time()
+deltatime = 0
+frameRate = 4
+
+xCoordiante = 4
+yCoordinate = 2
+yForce = 0
+
+mapp = [[1, 1, 0, 0, 0, 0, 0, 0],
+       [1, 0, 0, 0, 0, 0, 5, 0],
+       [1, 0, 0, 0, 0, 0, 0, 0],
+       [1, 0, 0, 0, 1, 0, 0, 0],
+       [1, 0, 0, 0, 1, 0, 0, 0],
+       [1, 0, 0, 0, 1, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0],
+       [1, 1, 0, 0, 0, 0, 0, 0]]
+
+#def SetLedToColor(x,y,color):
+#    neoPixel[x+y*8] = color
+#   neoPixel.write()
+
+
+def ApplyMovement(x):
+    global mapp, xCoordiante,yCoordinate
+    newX = (xCoordiante+x) % 8
+    if(mapp[newX][yCoordinate] != 0):
+        if(x > 0):
+            ApplyMovement(x-1)
+            return
+        if(x < 0):
+            ApplyMovement(x+1)
+            return
+
+
+    xCoordiante = newX;
+
+
+'''def Draw():
+    global colors,mapp, xCoordiante,yCoordinate
+    for x in range(0,8):
+        for y in range(0,0):
+            if(mapp[x][y]!= 5):
+                neoPixel[x + y * 8] = colors[mapp[x][y]]
+            if(mapp[x][y]== 5):
+                neoPixel[x + y * 8] = (random.randrange(0,255),random.randrange(0,255),random.randrange(0,255))
+
+
+    neoPixel[xCoordiante+ 8*yCoordinate] = colors[2]
+    neoPixel.write()'''
+
+def Update():
+    global mapp
+    move = 1
+   # if(button0.value() == 1):
+    #    move += 1
+    #if(button1.value() == 1):
+     #   move -= 1
+
+    ApplyMovement(move)
+    ApplyForce()
+    #Draw()
+    print(mapp)
+    print("x: ", xCoordiante, "y: ", yCoordinate)
+
+def ApplyForce():
+    global xCoordiante,yCoordinate,yForce,mapp
+    if(mapp[xCoordiante][(yCoordinate+1)%8] == 0):
+        yForce += 1
+    if(mapp[xCoordiante][(yCoordinate+1)%8] == 1):
+        yForce = 0
+
+    for x in range(0,abs(yForce)):
+        if(yForce > 0):
+            if(mapp[xCoordiante][(yCoordinate+1)%8] == 0):
+                yCoordinate=  (yCoordinate+1)%8
+        if(yForce < 0):
+            if(mapp[xCoordiante][(yCoordinate-1)%8] == 0):
+                yCoordinate = (yCoordinate - 1) % 8
+
+    if(yForce > 4):
+        yForce = 4
+
+
+
+def MainLoop():
+    global startTime,lastFrameTime,deltatime,frameRate
+    while(True):
+        if(time.time()-(1/frameRate) > lastFrameTime):
+            deltatime = time.time()-lastFrameTime
+            lastFrameTime = time.time()
+            Update()
+
+MainLoop()
